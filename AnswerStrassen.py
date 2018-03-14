6c356861723fe4db25fd5f5f54949c38ab664a10
AlfredoL!ma <alfredolms@ic.ufal.br>
2018-03-14 00:11:07 -0300

    Update AnswerStrassen.py

--- AnswerStrassen.py
+++ AnswerStrassen.py
@@ -2,6 +2,8 @@ import os
 import sys
 import optparse
 import importlib
+import csv
+import time
 
 def readFile( name , lineM ):
 	matrix = []
@@ -32,9 +34,6 @@ def readFiles( name_m1 , name_m2 ):
 
 	return matrix1 , matrix2
 
-
-l = ["Aluno1", "Aluno2"]
-
 def equal( m1 , m2 ):
 
 	if len(m1) != len(m2): return False
@@ -48,27 +47,52 @@ def equal( m1 , m2 ):
 
 	return True
 
+def createCsv(size):
+	file = open( 'Notas.csv' , 'w' )
+	file.write( 'matricula' )
+	for i in range(size):
+		file.write( ',time {}'.format(i+1) )
+	file.write( ',nota\n' )
 
-for i in l:
-	points = 0
-	try:
-		loader = importlib.import_module(i)
-	except Exception as e:
-		print('Error:', e)
-		continue
 
-	for j in range(1,11):
-		m1 , m2 = readFiles("../M1_{}.in".format(j),"../M2_{}.in".format(j))
-		try:
-			a = loader.Strassen( m1, m2 )
-		except Exception as e:
-			print('Caso {}: Error -'.format(j), e)
-			continue
-		m3 = readFile("../M3_{}.in".format(j), len(m1))
-		if( equal(m3,a) ):
-			print('Caso {}: OK'.format(j))
-			points += 1
-		else:
-			print('Caso {}: Error -'.format(j), "Diferente")
+def run(size):
+	files = os.listdir()
+	with open('Notas.csv', 'a') as data:
+		writer = csv.writer(data)
+		for i in files:
+			if 	i[-3:] != '.py' or i == 'rank.py' : 
+				continue
+			values = []
+			values.append( i[1:-3] )
+			points = 0
+			try:
+				loader = importlib.import_module(i[:-3])
+			except Exception as e:
+				for j in range(size): values.append( e )
+				values.append( 0 )
+				writer.writerow(values)
+				continue
 
-	print( i , points )
+			for j in range(1,size+1):
+				m1 , m2 = readFiles("../M1_{}.in".format(j),"../M2_{}.in".format(j))
+				t1 = time.time()
+				try:
+					a = loader.Strassen( m1, m2 )
+				except Exception as e:
+					values.append( e )
+					continue
+				t2 = time.time() 
+				m3 = readFile("../M3_{}.in".format(j), len(m1))
+				if( equal(m3,a) ):
+					values.append( str(t2 - t1)[:6] )
+					points += 1
+				else:
+					values.append( 'X' )
+			values.append(points)
+			writer.writerow(values)
+
+def main():
+	createCsv( 10 )
+	run( 10 )
+
+main()
